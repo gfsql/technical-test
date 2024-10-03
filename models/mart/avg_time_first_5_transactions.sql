0b777e9a32dbfd86582538fb06524eaf81e16154
Giedrius Siliauskis <giedrius.siliauskis@revelsystems.com>
2024-10-03 17:12:03 +0300

    Correct adoption rate

--- models/mart/avg_time_first_5_transactions.sql
+++ models/mart/avg_time_first_5_transactions.sql
@@ -1,13 +1,30 @@
-WITH first_5_transactions AS (
+WITH ranked_transactions AS (
     SELECT
         store_id,
         happened_at,
-        ROW_NUMBER() OVER (PARTITION BY store_id ORDER BY happened_at) AS transaction_number
+        ROW_NUMBER() OVER (PARTITION BY store_id ORDER BY happened_at) AS transaction_rank  -- Rank transactions per store
     FROM {{ ref('fct_transactions') }}
+),
+
+fiveth_transaction AS (
+    SELECT
+        store_id,
+        happened_at AS transaction_date  -- Get the date of the first transaction among the first five
+    FROM ranked_transactions
+    WHERE transaction_rank = 5  -- Only include the first five transactions
+),
+
+store_adoption AS (
+    SELECT
+        s.store_id,
+        s.created_at AS store_created_at,
+        fdt.transaction_date,
+        fdt.transaction_date - s.created_at AS adoption_time_days
+    FROM {{ ref('stg_store') }} s
+    JOIN fiveth_transaction fdt ON s.store_id = fdt.store_id
 )
+
 SELECT
-    store_id,
-    AVG(EXTRACT(EPOCH FROM happened_at)) AS avg_time_seconds
-FROM first_5_transactions
-WHERE transaction_number <= 5
-GROUP BY store_id
+    AVG(adoption_time_days) AS average_adoption_time_days,
+    COUNT(*) AS store_count
+FROM store_adoption
